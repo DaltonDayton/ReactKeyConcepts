7e15518282101d0857f7c4a0ee3c567d007e38ce
Dalton Dayton <daltondayton1@gmail.com>
2023-06-18 17:19:25 -0600

    Update Concept

--- src/components/Concepts/Concept.jsx
+++ src/components/Concepts/Concept.jsx
@@ -1,7 +1,7 @@
 const Concept = (props) => {
   return (
     <li className="concept">
-      <img src={props.concept.image} alt="TODO: TITLE" />
+      <img src={props.concept.image} alt={props.concept.title} />
       <h2>{props.concept.title}</h2>
       <p>{props.concept.description}</p>
     </li>
